adef49a0e91cfec1173eca732371cbe50f6352f2
林俊威 <2920155162@qq.com>
2023-10-15 16:01:34 +0800

    change2

--- Main1.java
+++ Main1.java
@@ -1,7 +1,7 @@
 
 public class Main1 {
 
-    public static void main(String[] args) { //这是第二次修改
+    public static void main(String[] args) { //这是第三次修改
         System.out.println("Are you OK?");
 
         System.out.println("yes,i am");
